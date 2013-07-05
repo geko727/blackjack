d3387dfc404527dde95057e29e70065c7a2ebc03
Carlos Manuel <geko727@gmail.com>
2013-07-04 23:29:44 -0500

    falta ingles

--- black.rb
+++ black.rb
@@ -28,6 +28,9 @@ def total player
  	while b <= player.length-1
  			if player[b][1] == 'A'
  				@total =  @total + 11
+ 				if @total > 21
+ 					@total =  @total - 10
+ 				end
  			else
  				@total =  @total + player[b][1]
  			end
@@ -35,7 +38,7 @@ def total player
 
  	end
  	return @total
- end
+end
 
 def mostrar player, casa
 	 total player
@@ -44,7 +47,47 @@ def mostrar player, casa
 	 puts "Casa: #{casa[0]}, *"
 end
  
+def tcasa casa, deck
+	total casa 
+    if @total < 17
+    	repartir deck, casa
+    	tcasa casa, deck
+    end
+    @total
+end
+
+def juego casa, deck, player, op, totplay
+	if op == 'stay'
+		tcasa casa, deck
+		puts "Casa: #{casa}, el total de la casa es de: #{@total}"
+		totcas = @total
+		if totcas > 21 
+			puts "Tu ganas, la casa rebaso el limite de 21"
+		elsif totcas <= 21 && totcas > totplay
+			puts "La casa gana"
+		elsif totcas <= 21 && totcas < totplay
+	        puts "Tu ganas, tus cartas son mayores a las de la casa"
+		elsif totcas <= 21 && totcas == totplay
+	        puts "empate"
+	    end
+	elsif op == 'hit'
+		repartir deck, player
+		mostrar player, casa
+		if @total > 21
+			puts "Tu total es de: #{@total}, pierdes por pasar el limite de 21"
+		elsif @total == 21
+			puts "Tu Ganas!!!! tu total es de 21"
+		else
+			puts "Tu total es de: #{@total}, sacas otra carta o te quedas? hit o stay"
+			op = gets.chomp
+			totplay = @total
+			juego casa, deck, player, op, totplay
+	    end
+	end	
+end
+
 # Primeras dos cartas
+
 repartir deck, player
 repartir deck, casa
 repartir deck, player
@@ -54,5 +97,13 @@ repartir deck, casa
 puts 'Inicia el juego'
 puts '---------------'
 mostrar player, casa
-puts "Tu total es de: #{@total}, sacas otra carta o te quedas?"
+puts '-------------------------------------------------'
+if @total == 21
+			puts "Tu Ganas!!!! tu total es de 21"
+else
+puts "Tu total es de: #{@total}, sacas otra carta o te quedas? hit o stay"
+totplay = @total
 op = gets.chomp
+juego casa, deck, player, op, totplay
+end
+
